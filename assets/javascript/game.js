ebe70d6fab8b6a01fe65f2b53aeadc114955dbd1
Damiano Raab <DamianoRaab11@gmail.com>
2018-10-09 19:03:44 -0400

    new files

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// Array of Words
+var word = [
+    "Hazard", 
+    "Mbappe", 
+    "Ronaldo", 
+    "Neymar", 
+    "Messi", 
+    "Dybala"
+];
+
+// Choose random Word
+var randomNum = Math.floor(Math.random() * word.length);
+var chosenWord = word[randomNum];
+var underScore = ["_"];
+// Underscores based on length of the Word
+
+// Get users guess
+
+// Check if Guess is right
+
+//If right push to right array
+
+//If wrong push to wrong array
