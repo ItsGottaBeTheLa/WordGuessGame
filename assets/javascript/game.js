bc3b15a035810f51400ef78797e57cbc60c43825
Damiano Raab <DamianoRaab11@gmail.com>
2018-10-11 18:56:47 -0400

    file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 // Grab reference to my DOM Elements
-var $newGameButton = document.getElementById("new-game-button");
-var $placeholders = document.getElementById("place-holders");
-var $guessedLetters = document.getElementById("guessed-letters");
-var $lettersLeft = document.getElementById("letters-left");
-var $wins = document.getElementById("wins-made");
-var $losses = document.getElementById("losses-made");
+var newGameButton = document.getElementById("new-game-button");
+var placeholders = document.getElementById("place-holders");
+var guessedLetters = document.getElementById("guessed-letters");
+var lettersLeft = document.getElementById("letters-left");
+var wins = document.getElementById("wins-made");
+var losses = document.getElementById("losses-made");
 
 
 // Create Variables for game
@@ -16,14 +16,13 @@ var pickedWord = " ";
 var pickedWordPlaceHolderArr = [];
 var guessedLetterBank = [];
 var incorrectLetterBank = [];
-var gameRunning = false;
+
 
 
 // Create new game function - to reset game
 function newGame() {
     
     //reset all game info
-    gameRunning = true;
     guessesLeft = 7;
     guessedLetterBank = [];
     incorrectLetterBank = [];
@@ -42,7 +41,9 @@ function newGame() {
     }
 
     // Write game info to DOM
-
+    guessesLeft.textContent = guessesLeft;
+    placeholders.textContent = pickedWordPlaceHolderArr.join("");
+    guessedLetters.textContent = incorrectLetterBank;
 
 }
 // letterGuess function, based on letter pressed if it is in the array
@@ -61,7 +62,7 @@ function newGame() {
 
 
 // add event listener for new game button
-$newGameButton.addEventListener("click", newGame);
+newGameButton.addEventListener("click", newGame);
 
 
 // add onkeyup event to trigger letterGuess
